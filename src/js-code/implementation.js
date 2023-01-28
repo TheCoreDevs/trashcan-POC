a84613e24ebc3fb4f474580e78d8f5cb0d6d68fd
Sheeeev66 <roi.hakim@gmail.com>
2023-01-28 15:57:35 +0200

    commit

--- src/js-code/implementation.js
+++ src/js-code/implementation.js
@@ -1,57 +1,60 @@
 import { ethers } from "ethers";
 
-// A Web3Provider wraps a standard Web3 provider, which is
-// what MetaMask injects as window.ethereum into each page
-const provider = new ethers.providers.Web3Provider(window.ethereum);
+window.addEventListener("load", () => {
 
-// MetaMask requires requesting permission to connect users accounts
-provider.send("eth_requestAccounts", []);
+    // A Web3Provider wraps a standard Web3 provider, which is
+    // what MetaMask injects as window.ethereum into each page
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-// The MetaMask plugin also allows signing transactions to
-// send ether and pay to change state within the blockchain.
-// For this, you need the account signer...
-const signer = provider.getSigner()
+    // MetaMask requires requesting permission to connect users accounts
+    provider.send("eth_requestAccounts", []);
 
-const trashcanABI = [
-    'function cleanERC721(address[] tokens, uint[][] ids)',
-    'function cleanERC1155(address[] tokens, uint[][] ids, uint[][] amounts)',
-    'function cleanERC20(address[] memory tokens)'
-]
+    // The MetaMask plugin also allows signing transactions to
+    // send ether and pay to change state within the blockchain.
+    // For this, you need the account signer...
+    const signer = provider.getSigner()
 
-const trashcanAddress = "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85"; // Add smart contract address here
+    const trashcanABI = [
+        'function cleanERC721(address[] tokens, uint[][] ids)',
+        'function cleanERC1155(address[] tokens, uint[][] ids, uint[][] amounts)',
+        'function cleanERC20(address[] memory tokens)'
+    ]
 
-const trashcanContract = new ethers.Contract(trashcanAddress, trashcanABI, provider);
-const trashcanContractWithSigner = trashcanContract.connect(signer);
+    const trashcanAddress = "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85"; // Add smart contract address here
 
-window.cleanERC721 = async function() {
-    const tokens = document.getElementById("tokens721").value.split(",");
-    const ids = document.getElementById("ids721").value.split(",");
-    try {
-        await trashcanContractWithSigner.cleanERC721(tokens, ids);
-        document.getElementById("status").innerHTML = "Tokens cleaned successfully!";
-    } catch (err) {
-        document.getElementById("status").innerHTML = "Error cleaning tokens: " + err;
+    const trashcanContract = new ethers.Contract(trashcanAddress, trashcanABI, provider);
+    const trashcanContractWithSigner = trashcanContract.connect(signer);
+
+    window.cleanERC721 = async function() {
+        const tokens = document.getElementById("tokens721").value.split(",");
+        const ids = document.getElementById("ids721").value.split(",");
+        try {
+            await trashcanContractWithSigner.cleanERC721(tokens, ids);
+            document.getElementById("status").innerHTML = "Tokens cleaned successfully!";
+        } catch (err) {
+            document.getElementById("status").innerHTML = "Error cleaning tokens: " + err;
+        }
     }
-}
 
-window.cleanERC1155 = async function() {
-    const tokens = document.getElementById("tokens1155").value.split(",");
-    const ids = document.getElementById("ids1155").value.split(",");
-    const amounts = document.getElementById("amounts1155").value.split(",");
-    try {
-        await trashcanContractWithSigner.cleanERC1155(tokens, ids, amounts);
-        document.getElementById("status").innerHTML = "Tokens cleaned successfully!";
-    } catch (err) {
-        document.getElementById("status").innerHTML = "Error cleaning tokens: " + err;
+    window.cleanERC1155 = async function() {
+        const tokens = document.getElementById("tokens1155").value.split(",");
+        const ids = document.getElementById("ids1155").value.split(",");
+        const amounts = document.getElementById("amounts1155").value.split(",");
+        try {
+            await trashcanContractWithSigner.cleanERC1155(tokens, ids, amounts);
+            document.getElementById("status").innerHTML = "Tokens cleaned successfully!";
+        } catch (err) {
+            document.getElementById("status").innerHTML = "Error cleaning tokens: " + err;
+        }
     }
-}
 
-window.cleanERC20 = async function() {
-    const tokens = document.getElementById("tokens20").value.split(",");
-    try {
-        await trashcanContractWithSigner.cleanERC20(tokens);
-        document.getElementById("status").innerHTML = "Tokens cleaned successfully!";
-    } catch (err) {
-        document.getElementById("status").innerHTML = "Error cleaning tokens: " + err;
+    window.cleanERC20 = async function() {
+        const tokens = document.getElementById("tokens20").value.split(",");
+        try {
+            await trashcanContractWithSigner.cleanERC20(tokens);
+            document.getElementById("status").innerHTML = "Tokens cleaned successfully!";
+        } catch (err) {
+            document.getElementById("status").innerHTML = "Error cleaning tokens: " + err;
+        }
     }
-}
+});
