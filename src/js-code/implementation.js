72a3af19d8e366e3a44cd38cc50a0aa5a3368470
Sheeeev66 <roi.hakim@gmail.com>
2023-01-29 12:11:29 +0200

    Update implementation.js

--- src/js-code/implementation.js
+++ src/js-code/implementation.js
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 
-window.addEventListener("load", () => {
+document.addEventListener("DOMContentLoaded", () => {
 
     // A Web3Provider wraps a standard Web3 provider, which is
     // what MetaMask injects as window.ethereum into each page
